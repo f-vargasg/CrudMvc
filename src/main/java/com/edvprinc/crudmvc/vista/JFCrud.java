43164b29a1a8d132c6b196a4d43df1121411427a
Francisco Vargas G. <52474105+f-vargasg@users.noreply.github.com>
2020-08-26 21:41:15 -0600

    Se optimizó la clase

--- src/main/java/com/edvprinc/crudmvc/vista/JFCrud.java
+++ src/main/java/com/edvprinc/crudmvc/vista/JFCrud.java
@@ -10,8 +10,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
 
 /**
  *
